f6a9b1a53b63edaafde545caac5d5405d8bfac16
Kane Quinlan <longbottomk@outlook.com>
2026-03-20 13:27:29 +0000

    feat: remove module boilerplate

--- modules/expo-play-games/index.ts
+++
@@ -1,5 +0,0 @@
-// Reexport the native module. On web, it will be resolved to PlayGamesModule.web.ts
-// and on native platforms to PlayGamesModule.ts
-export { default } from './src/PlayGamesModule';
-export { default as PlayGamesModuleView } from './src/PlayGamesModuleView';
-export * from  './src/PlayGamesModule.types';
